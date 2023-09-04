c03b75e559f14a459e1e09aaf494c1193de1ffad
Ahsan-Masood98 <ahsanmasood795@gmail.com>
2023-09-04 14:36:20 +0500

    form handling and some secrity tips in php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="includes/formhandler.php" method="post">
+        <label for="firstname">Firstname</label>
+        <input type="text" id="firstname" name="firstname" placeholder="firstname">
+        <label for="firstname">Lastname</label>
+        <input type="text" id="lastname" name="lastname" placeholder="lastname">
+        <select name="favouritepet" id="favouritepet">
+            <option value="none">none</option>
+            <option value="cat">cat</option>
+            <option value="bird">bird</option>
+        </select>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+
+</html>
