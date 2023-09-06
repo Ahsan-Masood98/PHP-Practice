62d80372646f4ba33f3df64808e737c8366cba9b
Ahsan-Masood98 <ahsanmasood795@gmail.com>
2023-09-06 15:36:22 +0500

    #

--- index.php
+++ index.php
@@ -163,27 +163,6 @@
     }
     ?>
 
-    <h3>Sign Up</h3>
-    <form action="includes/formhandler.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="password" placeholder="Password">
-        <input type="email" name="email" placeholder="Email">
-        <button>Signup</button>
-    </form>
-    <h3>Update User</h3>
-    <form action="includes/userupdate.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="password" placeholder="Password">
-        <input type="email" name="email" placeholder="Email">
-        <button>Update</button>
-    </form>
-    <h3>Delete User</h3>
-    <form action="includes/userdelete.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="password" placeholder="Password">
-        <button>Delete</button>
-    </form>
-
 </body>
 
 </html>
