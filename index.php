d14b9495946f79dee2f85a2cd1bce5f85b5e2a46
Ahsan-Masood98 <ahsanmasood795@gmail.com>
2023-09-07 17:41:09 +0500

    Implemented logout functionality

--- index.php
+++ index.php
@@ -14,25 +14,39 @@ require_once './includes/config_session.inc.php';
 </head>
 
 <body>
-    <h3>Log In</h3>
-    <form action="includes/login.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password">
-        <?php
-        check_login_errors();
-        ?>
-        <button>Login</button>
-    </form>
-    <h3>Sign Up</h3>
-    <form action="includes/signup.inc.php" method="post">
-        <?php
-        signup_input();
-        ?>
-        <?php
-        check_signup_errors();
-        ?>
-        <button>Signup</button>
-    </form>
+    <h3><?php output_username() ?></h3>
+    <?php
+    if (!isset($_SESSION["user_id"])) { ?>
+        <h3>Log In</h3>
+        <form action="includes/login.inc.php" method="post">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="pwd" placeholder="Password">
+            <?php
+            check_login_errors();
+            ?>
+            <button>Login</button>
+        </form>
+        <h3>Sign Up</h3>
+        <form action="includes/signup.inc.php" method="post">
+            <?php
+            signup_input();
+            ?>
+            <?php
+            check_signup_errors();
+            ?>
+            <button>Signup</button>
+        </form>
+    <?php }
+    ?>
+
+    <?php
+    if (isset($_SESSION["user_id"])) { ?>
+        <h3>Log Out</h3>
+        <form action="includes/logout.inc.php" method="post">
+            <button>Logout</button>
+        </form>
+    <?php }
+    ?>
 </body>
 
 </html>
